f4ef54825259fb99e2692644933a67d2cab89421
123-Game <aefoster74@gmail.com>
2024-02-17 00:18:23 +0000

    Created getplaces.ts

---
+++ src/modules/getplaces.ts
@@ -0,0 +1,31 @@
+import * as vscode from "vscode";
+
+const getPlaces = (context: vscode.ExtensionContext): any => {
+    const globalStorageUri = context.globalStorageUri;
+    const filePath = vscode.Uri.joinPath(globalStorageUri, "data.json");
+
+    return vscode.workspace.fs.readFile(filePath).then((rawData) => {
+        const data = JSON.parse(rawData.toString());
+		const placesKeys = Object.keys(data);
+
+        let places = [
+            {
+                label: "Add place",
+                description: "Add a new place"
+            }
+        ];
+
+        for (const key of placesKeys) {
+            const place = data[key];
+    
+            places.push({
+                label: place.name,
+                description: place.placeId
+            });
+        }
+
+        return places;
+    });
+};
+
+export = getPlaces;
