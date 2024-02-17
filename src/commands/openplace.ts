a5ea52797faba54af99acaca6400dbc04609b708
123-Game <aefoster74@gmail.com>
2024-02-17 00:18:59 +0000

    Added game selection

---
+++ src/commands/openplace.ts
@@ -0,0 +1,66 @@
+import * as vscode from "vscode";
+
+import getGameInfo from "../modules/getgameinfo";
+import createExplorer from "../modules/createexplorer";
+import savePlace from "../modules/saveplace";
+import getPlaces from "../modules/getplaces";
+import getAPIkey from "../modules/getapikey";
+
+const openPlace = async (context: vscode.ExtensionContext) => {
+    let gameInfo: any;
+	let apiKey = "";
+	let placeId = "";
+
+    const placesArray = await getPlaces(context);
+
+    const places: vscode.QuickPickItem[] = placesArray;
+
+    const selection = await vscode.window.showQuickPick(places, { placeHolder: "Search places" });
+
+    if (selection === undefined) {
+        return;
+    } else if (selection.label === "Add place" && selection.description === "Add a new place") {
+        const tempPlaceId = await vscode.window.showInputBox({
+            placeHolder: "Place ID"
+        });
+
+        if (typeof (tempPlaceId) === "string" && tempPlaceId.length > 0) {
+            placeId = tempPlaceId;
+        } else {
+            return;
+        }
+
+        const tempApiKey = await vscode.window.showInputBox({
+            placeHolder: "API Key"
+        });
+
+        if (typeof (tempApiKey) === "string" && tempApiKey.length > 0) {
+            apiKey = tempApiKey;
+        } else {
+            return;
+        }
+
+        gameInfo = await getGameInfo(placeId);
+
+        savePlace(context, gameInfo, placeId, apiKey);
+    } else if (selection.label && selection.description) {
+        placeId = selection.description;
+        apiKey = await getAPIkey(context, placeId);
+    }
+
+    if (gameInfo === undefined) {
+        gameInfo = await getGameInfo(placeId);
+    }
+
+    vscode.window.showInformationMessage("Loading place");
+
+    createExplorer(context, gameInfo, placeId, apiKey);
+
+    return {
+        gameInfo: gameInfo,
+        apiKey: apiKey,
+        placeId: placeId
+    };
+};
+
+export = openPlace;
