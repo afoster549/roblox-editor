a313f99a3dd06d1792ea4e5bbf44a75381c407ad
123-Game <aefoster74@gmail.com>
2024-02-17 00:23:17 +0000

    Added check for pre-existing editor

--- src/commands/openeditor.ts
+++ src/commands/openeditor.ts
@@ -1,6 +1,10 @@
 import * as vscode from "vscode";
 
 const openEditor = async (instance: any, script: string, editors: any) => {
+    if (editors[instance.engineInstance.Id] !== undefined) {
+        return;
+    }
+
     const doc = await vscode.workspace.openTextDocument(vscode.Uri.parse(`untitled:${instance.engineInstance.Name} - ${script}`));
 
     await vscode.languages.setTextDocumentLanguage(doc, "lua");
