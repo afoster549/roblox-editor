793fb3531485ecf478bd0f34b3bc269810dfea22
123-Game <aefoster74@gmail.com>
2024-02-17 00:54:02 +0000

    Added ability to remove places

--- src/modules/saveplace.ts
+++ src/modules/saveplace.ts
@@ -1,24 +1,34 @@
 import * as vscode from "vscode";
 
-const savePlace = (context: vscode.ExtensionContext, gameInfo: any, placeId: string, apiKey: string) => {
+const savePlace = (context: vscode.ExtensionContext, remove: boolean, gameInfo?: any, placeId?: string, apiKey?: string) => {
     const globalStorageUri = context.globalStorageUri;
     const filePath = vscode.Uri.joinPath(globalStorageUri, "data.json");
-
-    const placeData = {
-        "name": gameInfo.name,
-        "universeId": String(gameInfo.id),
-        "placeId": placeId,
-        "apiKey": apiKey
-    };
 
     let data: any;
 
     vscode.workspace.fs.readFile(filePath).then((rawData) => {
         data = JSON.parse(rawData.toString());
 
-        data[placeData.placeId] = placeData;
+        if (remove === false && placeId) {
+            console.log("huh");
+
+            const placeData = {
+                "name": gameInfo.name,
+                "universeId": String(gameInfo.id),
+                "placeId": placeId,
+                "apiKey": apiKey
+            };
+
+            data[placeId] = placeData;
+        } else if (remove && placeId) {
+            console.log("wat");
+
+            delete data[placeId];
+        }
 
         vscode.workspace.fs.writeFile(filePath, Buffer.from(JSON.stringify(data))).then(() => {
+            console.log(filePath);
+
             vscode.window.showInformationMessage("Saved place info.");
         });
     });
