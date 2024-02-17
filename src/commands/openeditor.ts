283bf1f59d5859ccea0e3eccee763a3f1824f024
123-Game <aefoster74@gmail.com>
2024-02-17 00:19:47 +0000

    Created openeditor.ts

---
+++ src/commands/openeditor.ts
@@ -0,0 +1,27 @@
+import * as vscode from "vscode";
+
+const openEditor = async (instance: any, script: string, editors: any) => {
+    const doc = await vscode.workspace.openTextDocument(vscode.Uri.parse(`untitled:${instance.engineInstance.Name} - ${script}`));
+
+    await vscode.languages.setTextDocumentLanguage(doc, "lua");
+
+    const editor = await vscode.window.showTextDocument(doc);
+
+    editor.edit(editBuilder => {
+        editBuilder.insert(new vscode.Position(0, 0,), instance.engineInstance.Details[script].Source);
+    });
+
+    editors[instance.engineInstance.Id] = {
+        instance: instance,
+        editor: editor,
+        type: script
+    };
+
+    return {
+        editors: editors,
+        editor: editor,
+        key: instance.engineInstance.Id
+    };
+};
+
+export = openEditor;
